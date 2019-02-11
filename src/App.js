c7944ff3b8ebe2d24391689ec17105b5da100a42
Robert Mills <robertmills1985@yahoo.com>
2019-02-11 15:56:21 -0700

    start over

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import SignUp from './components/SignUp/SignUp'
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SignUp email={'Enter your email.'} password={'Enter a password.'}/>
+      high
       </div>
     );
   }
