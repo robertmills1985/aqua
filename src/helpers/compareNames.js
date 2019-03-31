64475557c50dbf1d9ec819c965beb71d8289ac6b
Robert Mills <robertmills1985@yahoo.com>
2019-03-30 22:23:25 -0700

    created a function that takes in two strings and compares them, checking to if they are in abc value and returning a boolean

--- src/helpers/compareNames.js
+++ src/helpers/compareNames.js
@@ -54,27 +54,36 @@ function letterValue(input) {
 
 function compareNames(a, b) {
 	var shortest = 0;
+	var longest = 0;
 	if (a.length < b.length) {
 		shortest = a.length;
+		longest = b.length
 	} else {
 		shortest = b.length;
+		longest = a.length;
 	}
 	var aValue = 0;
 	var bValue = 0;
-	for (var i = 0; i < shortest; i++) {
-		console.log(letterValue(a[i]));
-		console.log(letterValue(b[i]));
+	for (var i = 0; i < longest; i++) {
+		//console.log(letterValue(a[i]));
+		//console.log(letterValue(b[i]));
 		aValue = aValue + letterValue(a[i]);
 		bValue = bValue + letterValue(b[i]);
 		if (aValue > bValue) {
-			console.log('false on iterations ' + i);
-			console.log('names out of order')
+			//console.log('false on iterations ' + i);
+			console.log('names OUT of order', i)
 			return false;
 		}
-		else if( i === shortest - 1){
-			console.log('names are in order')
+		else if(i === longest-1 && aValue <= bValue) {
+			console.log('names IN order', i)
+			if(aValue === bValue){
+				console.log('SAME values')
+			}
 			return true
 		}
+		
 	}
 }
-compareNames('aaaad', 'aaaac');
+compareNames('piss', 'shit');
+compareNames('alma','arizona')
+compareNames('hello','hello')
