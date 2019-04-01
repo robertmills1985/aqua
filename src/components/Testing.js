398cda7ed9307990f7b669eea07d113e70391b92
Robert Mills <robertmills1985@yahoo.com>
2019-03-31 19:34:43 -0700

    having some bugs pop up in the compareNames function, thinking about starting from scratch

--- src/components/Testing.js
+++ src/components/Testing.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { generate } from '../helpers/generate';
+import { checkForLetters } from "../helpers/checkForLetters";
+import {abcOrder} from '../helpers/abcOrder'
+
+var example = ['Sean Aniston','Bob Belcher','Elen Degenerojus','Bob Cole']
 
 const Test = (props) => (
 	<div>
 		<input id="test-input" placeholder="enter something..." />
         <button onClick={()=>generate(document.getElementById('test-input').value)}>generate</button>
+        <button onClick={()=>checkForLetters(document.getElementById('test-input').value)}>input check</button>
+		<button onClick={()=>abcOrder(example)}>test abcOrder</button>
 	</div>
 );
 
---
+++ src/helpers/abcOrder.js
@@ -0,0 +1,34 @@
+import { compareNames } from "./compareNames";
+
+//import {compareNames} from '../helpers/compareNames'
+
+var example = ['John Stuart','Arron Mason','Zack Effron','Art Masona', 'Rob Arizona', 'Gavin Belson', 'Chew Charleston']
+
+function getLastName(input){
+    var lastName = input.split(' ')[1]
+    return lastName
+}
+
+function abcOrder(input){
+    var inputCopy = input
+    //console.log(inputCopy)
+    for(var i = 0; i < input.length; i++){
+        //console.log(constant)
+        for(var x = 0; x < input.length; x++){
+            var constant = getLastName(inputCopy[i])
+            var comparison = getLastName(inputCopy[x])
+            var a = inputCopy[i]
+            var b = inputCopy[x]
+            if(compareNames(constant,comparison) != true){
+                console.log(constant + ' ' + comparison + ' = false')
+            }
+            else {
+                console.log(constant + ' ' + comparison + ' = true')
+            }
+        }
+    }
+    
+    //console.log('new array is ' + inputCopy)
+}
+//abcOrder(example)
+export {abcOrder}
