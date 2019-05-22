40727b4bc17d2116708c6defe271fcecb977fe06
Robert Mills <robertmills1985@yahoo.com>
2019-05-22 02:18:40 -0700

    ive added more to the generate() arrays and made it more diverse. Really just fun making it work

--- src/components/Testing.js
+++ src/components/Testing.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import {runTest} from '../testing'
 
 const Test = (props) => (
 	<div>
 		<input id="test-input" placeholder="enter something..." />
         <button onClick={props.generate}>generate</button>
-		<button onClick={props.abc}>Alphabetical Order</button>
-		<div>
-			<p>Running tests button requires 4 arguments seperated by commas.</p>
-			<p>First argument is how many rounds to run. Second is how many sets per round</p>
-			<p>The third argument will toggle the view for individual tests</p>
-			<p>The fourth argument will toggle abcOrder() on or off</p>
-			<p>example1: 3,5,on,off</p>
-			<p>example2: 10,100,on,on</p>
-			<button onClick={()=>runTest(document.getElementById('test-input').value )}>Run tests</button>
-		</div>
-		
+		<button onClick={props.abc}>alphabetical order</button>
 	</div>
 );
 
